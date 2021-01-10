6470799379b2855e0e6409027385b7c4dffc85fd
merlin <merlinarer@gmail.com>
2021-01-10 20:40:55 +0800

    merge train&val_collate to collate

--- utils/collate.py
+++ utils/collate.py
@@ -7,22 +7,6 @@ def collate(batch):
         imgs.append(sample[0])
         boxs.append(torch.FloatTensor(sample[1]))
         labels.append(torch.LongTensor(sample[2]))
-    return torch.stack(imgs, 0), boxs, labels
-
-
-def val_collate(batch):
-    imgs = []
-    boxs = []
-    labels = []
-    imgname = []
-    h = []
-    w = []
-    for sample in batch:
-        imgs.append(sample[0])
-        boxs.append(torch.FloatTensor(sample[1]))
-        labels.append(torch.LongTensor(sample[2]))
-        h.append(sample[3])
-        w.append(sample[4])
-        imgname.append(sample[5])
-    return torch.stack(imgs, 0), boxs, labels, h, w, imgname
+        info.append(sample[3])
+    return torch.stack(imgs, 0), boxs, labels, info
 
