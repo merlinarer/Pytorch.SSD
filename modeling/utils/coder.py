8942db1e9637e4e5ec34dcf5e49bf3bdf5f8749f
cohesion98 <827401265@qq.com>
2021-02-02 15:16:06 +0800

    correct encode with new loss function

--- modeling/utils/coder.py
+++ modeling/utils/coder.py
@@ -5,8 +5,8 @@ def decode(loc, priors, variances=None):
     """Decode locations from predictions to bb.
     Args:
         loc (tensor): Shape: [num_priors,4]
-        priors (tensor): Shape: [num_priors,4].
-        variances: (list[float]) Variances of priorboxes
+        priors (tensor): Shape: [num_priors,4].  xywh
+        variances: (list[float]) Variances of priorboxes xyxy
     """
     if variances is None:
         variances = [1, 1]
@@ -24,7 +24,7 @@ def encode(matched,
     """Encode locations from bb cords to model output.
         Args:
             matched (tensor): Shape: [num_priors,4]
-            priors (tensor): Shape: [num_priors,4].
+            priors (tensor): Shape: [num_priors,4].  xywh
             variances: (list[float])
         """
     if variances is None:
@@ -32,5 +32,24 @@ def encode(matched,
     g_cxcy = (matched[:, :2] + matched[:, 2:]) / 2 - priors[:, :2]
     g_cxcy /= (variances[0] * priors[:, 2:])
     g_wh = (matched[:, 2:] - matched[:, :2]) / priors[:, 2:]
+    g_wh = torch.log(g_wh) / variances[1]
+    return torch.cat([g_cxcy, g_wh], 1)  # [num_priors,4]
+
+def encode_xyxy(matched,
+           priors,
+           variances=None):
+    """Encode locations from bb cords to model output.
+        Args:
+            matched (tensor): Shape: [num_priors,4]  gt xyxy
+            priors (tensor): Shape: [num_priors,4].  anchor xyxy
+            variances: (list[float])
+        """
+    if variances is None:
+        variances = [1, 1]
+    p_xy = (priors[:, :2] +priors[:,2:]) /2
+    p_wh = (priors[:, 2:] - priors[:, :2])
+    g_cxcy = (matched[:, :2] + matched[:, 2:]) / 2 - p_xy
+    g_cxcy /= (variances[0] * p_wh)
+    g_wh = (matched[:, 2:] - matched[:, :2]) / p_wh
     g_wh = torch.log(g_wh) / variances[1]
     return torch.cat([g_cxcy, g_wh], 1)  # [num_priors,4]
